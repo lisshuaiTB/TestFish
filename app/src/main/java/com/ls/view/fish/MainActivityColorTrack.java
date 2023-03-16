3acf90bbf1741c2d28da4fd4b44cb824912ca57f
liss <lishuai@testbird.com>
2023-03-16 22:12:03 +0800

    add

---
+++ app/src/main/java/com/ls/view/fish/MainActivityColorTrack.java
@@ -0,0 +1,42 @@
+package com.ls.view.fish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivityColorTrack extends AppCompatActivity {
+
+    private ColorTrackTextView mColorTrackTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_text);
+        mColorTrackTextView = findViewById(R.id.color_track_tv);
+    }
+
+    public void leftToRight(View view) {
+        setAnimation(ColorTrackTextView.Direction.LEFT_TO_RIGHT);
+    }
+
+    public void rightToLeft(View view) {
+        setAnimation(ColorTrackTextView.Direction.RIGHT_TO_LEFT);
+    }
+
+    public void setAnimation(ColorTrackTextView.Direction direction) {
+        mColorTrackTextView.setDirection(direction);
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(2000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float currentProgress = (float) animation.getAnimatedValue();
+                mColorTrackTextView.setCurrentProgress(currentProgress);
+            }
+        });
+        valueAnimator.start();
+    }
+}
